985063f65056cd3601d10da56bc307adfa142e0e
Wong Wei Jun <wong.weijun923@gmail.com>
2026-06-21 21:38:08 +0800

    Initial commit

---
+++ backend/app/api/routes/posts.py
@@ -0,0 +1,14 @@
+from datetime import timedelta
+from typing import Annotated, Any
+
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.responses import HTMLResponse
+from fastapi.security import OAuth2PasswordRequestForm
+
+from app import crud
+from app.api.deps import CurrentUser, SessionDep, get_current_active_superuser
+from app.core.config import settings
+from app.models import Message, NewPassword, Token, UserPublic, UserUpdate
+
+
+router = APIRouter(tags=["posts"])
