88ed35b12c87d488e0329dd5a85b4b7e0720b98d
Wong Wei Jun <wong.weijun923@gmail.com>
2026-06-26 21:47:20 +0800

    made email generated to be random

--- backend/tests/api/routes/test_private.py
+++ backend/tests/api/routes/test_private.py
@@ -5,11 +5,16 @@ from app.core.config import settings
 from app.models import User
 
 
+from tests.utils.utils import random_email
+
+
 def test_create_user(client: TestClient, db: Session) -> None:
+    email = random_email()
+
     r = client.post(
         f"{settings.API_V1_STR}/private/users/",
         json={
-            "email": "pollo@listo.com",
+            "email": email,
             "password": "password123",
             "full_name": "Pollo Listo",
         },
@@ -22,5 +27,5 @@ def test_create_user(client: TestClient, db: Session) -> None:
     user = db.exec(select(User).where(User.id == data["id"])).first()
 
     assert user
-    assert user.email == "pollo@listo.com"
+    assert user.email == email
     assert user.full_name == "Pollo Listo"
