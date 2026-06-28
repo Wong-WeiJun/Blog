2343f165bf0d9d8eed61e8f0e228543348554e25
Wong Wei Jun <wong.weijun923@gmail.com>
2026-06-28 21:40:35 +0800

    added avatr url

--- backend/app/models.py
+++ backend/app/models.py
@@ -17,6 +17,7 @@ class UserBase(SQLModel):
     is_active: bool = True
     is_superuser: bool = False
     full_name: str | None = Field(default=None, max_length=255)
+    avatar_url: str | None = Field(default=None, max_length=1000)
 
 
 # Properties to receive via API on creation
@@ -39,6 +40,7 @@ class UserUpdate(UserBase):
 class UserUpdateMe(SQLModel):
     full_name: str | None = Field(default=None, max_length=255)
     email: EmailStr | None = Field(default=None, max_length=255)
+    avatar_url: str | None = Field(default=None, max_length=1000)
 
 
 class UpdatePassword(SQLModel):
