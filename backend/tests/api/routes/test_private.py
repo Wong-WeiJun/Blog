ca924e4521fb040a3898c750614803c4370184fe
Wong Wei Jun <wong.weijun923@gmail.com>
2026-06-28 21:41:01 +0800

    added specific

--- backend/tests/api/routes/test_private.py
+++ backend/tests/api/routes/test_private.py
@@ -3,16 +3,13 @@ from sqlmodel import Session, select
 
 from app.core.config import settings
 from app.models import User
-from tests.utils.utils import random_email
 
 
 def test_create_user(client: TestClient, db: Session) -> None:
-    email = random_email()
-
     r = client.post(
         f"{settings.API_V1_STR}/private/users/",
         json={
-            "email": email,
+            "email": "pollo@listo.com",
             "password": "password123",
             "full_name": "Pollo Listo",
         },
@@ -25,5 +22,5 @@ def test_create_user(client: TestClient, db: Session) -> None:
     user = db.exec(select(User).where(User.id == data["id"])).first()
 
     assert user
-    assert user.email == email
+    assert user.email == "pollo@listo.com"
     assert user.full_name == "Pollo Listo"
