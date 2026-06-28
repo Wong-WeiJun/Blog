76f3f5022b78c39f2812c904d3849dbeaa361ba5
Wong Wei Jun <wong.weijun923@gmail.com>
2026-06-28 21:41:08 +0800

    updated exec

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -7,7 +7,7 @@ from sqlmodel import Session, delete
 from app.core.config import settings
 from app.core.db import engine, init_db
 from app.main import app
-from app.models import Post, User
+from app.models import Post, PostTagLink, User
 from tests.utils.user import authentication_token_from_email
 from tests.utils.utils import get_superuser_token_headers
 
@@ -17,9 +17,9 @@ def db() -> Generator[Session, None, None]:
     with Session(engine) as session:
         init_db(session)
         yield session
-
-        session.exec(delete(Post))
-        session.exec(delete(User))
+        session.execute(delete(PostTagLink))
+        session.execute(delete(Post))
+        session.execute(delete(User))
         session.commit()
 
 
