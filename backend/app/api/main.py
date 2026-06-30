0793b0a659aa578d68ecc3d02b926887a3fe5236
Wong Wei Jun <wong.weijun923@gmail.com>
2026-06-30 20:39:09 +0800

    added tags

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -9,6 +9,7 @@ api_router.include_router(users.router)
 api_router.include_router(utils.router)
 api_router.include_router(posts.router, prefix="/posts")
 api_router.include_router(uploads.router, prefix="/uploads")
+api_router.include_router(tags.router, prefix="/tags")
 
 
 if settings.ENVIRONMENT == "local":
