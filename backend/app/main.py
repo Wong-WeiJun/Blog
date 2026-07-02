305d88c6c08274666dee94948c4c0864822bd046
Wong Wei Jun <wong.weijun923@gmail.com>
2026-07-02 12:08:36 +0800

    added fallback

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,7 +7,7 @@ from app.core.config import settings
 
 
 def custom_generate_unique_id(route: APIRoute) -> str:
-    return f"{route.tags[0]}-{route.name}"
+    return f"{route.tags[0] if route.tags else 'no-tag'}-{route.name}"
 
 
 app = FastAPI(
