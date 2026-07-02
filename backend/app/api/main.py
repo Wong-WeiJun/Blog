51165d97fda44700c2d681c0f2652e40512f7730
Wong Wei Jun <wong.weijun923@gmail.com>
2026-07-02 21:42:42 +0800

    added contact

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,6 +1,16 @@
 from fastapi import APIRouter
 
-from app.api.routes import comments, login, posts, private, tags, uploads, users, utils
+from app.api.routes import (
+    comments,
+    login,
+    posts,
+    private,
+    tags,
+    uploads,
+    users,
+    utils,
+    contact,
+)
 from app.core.config import settings
 
 api_router = APIRouter()
@@ -11,6 +21,7 @@ api_router.include_router(posts.router, prefix="/posts")
 api_router.include_router(uploads.router, prefix="/uploads")
 api_router.include_router(tags.router, prefix="/tags")
 api_router.include_router(comments.router)
+api_router.include_router(contact.router, prefix="/contact")
 
 
 if settings.ENVIRONMENT == "local":
